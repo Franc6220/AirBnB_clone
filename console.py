4ec30dc0a0d50535cbaebfae17ee366ece54050d
Franc6220 <samuelwangalibo@yahoo.com>
2024-03-08 10:29:45 -0800

    console

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin
+#contains the entry point of the command interpreter
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+	#Defines Command Interpreter
+
+	prompt = "(hbnb)"
+
+	def emptyline(self):
+		#shouldn’t execute anything
+		pass
+
+	def do_quit(self, arg):
+		#to exit the program
+		return TRUE
+
+	def do_EOF(self, arg):
+		#End Of File
+		return TRUE	
+
+
+	if __name__ == '__main__':
+		HBNBCommand().cmdloop()
