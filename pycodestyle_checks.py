ab04db6d034bce203aa95d431ee32b4a49a046b0
Franc6220 <samuelwangalibo@yahoo.com>
2024-03-10 08:54:45 -0700

    code_style

--- pycodestyle_checks.py
+++ pycodestyle_checks.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-""" This program prints Hello, world! """
+#This program calculates the variance of numbers
+def calculate_variance(numbers):
+    sum_numbers = 0
+    for number in numbers:
+        sum_numbers = sum_numbers + number
+    mean = sum_numbers / len(numbers)
 
-print('Hello, world!')
+#Calculates the mean of squared numbers
+    sum_squares = 0
+    for number in numbers:
+        sum_squares = sum_squares + number**2
+    mean_squares = sum_squares / len(numbers)
 
+    return mean_squares - mean**2
