a07581185a4801bc7e9dbc8ba822f07c26face36
Franc6220 <samuelwangalibo@yahoo.com>
2024-03-13 12:44:01 -0700

    module

--- models/base_model.py
+++ models/base_model.py
@@ -4,22 +4,38 @@
 from uuid import uuid4
 from datetime import datetime
 
+
+instance1 = ["{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow"]
+instance2 = ["{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow"]
+
+
 class BaseModel:
 	#Represents base_model of HBnB project
+
 
 	def __init__(self, id, created_at, updated_at):
 		#Public instance attributes
 		self.id = id
 		self.created_at = created_at
 		self.updated_at = updated_at
-		
-		instance = BaseModel("{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow")
-		print(instance.id, instance.created_at, instance.updated_at)
+
+
+	def code(self):
+		print(f"{self.id}, {self.created_at}, {self.updated_at} is first instance")
+
+
+		instance1 = BaseModel("{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow")
+		instance2 = BaseModel("{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow")
+
+		instance1.code()
+		instance2.code()
+
 
 	def save(self):
 		#Modifies updated_at with current datetime. 
 		self.updated_at = datetime.utcnow()
 		models.storage.save()
+
 
 	def to_dict(self):
 		#Creates a dict of the self-set values
@@ -28,6 +44,7 @@ class BaseModel:
 		return_dict["created_at"] = self.created_at.isoformat()
 		return_dict["updated_at"] = self.updated_at.isoformat()
 		return return_dict
+
 
 	def __str__(self):
 		#Returns str representation of the BaseModel
