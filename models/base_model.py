c1d576aaf3ad0aab7d93295381e864435519214d
Franc6220 <samuelwangalibo@yahoo.com>
2024-05-25 09:00:10 -0700

    Base

--- models/base_model.py
+++ models/base_model.py
@@ -5,48 +5,32 @@ from uuid import uuid4
 from datetime import datetime
 
 
-instance1 = ["{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow"]
-instance2 = ["{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow"]
-
-
 class BaseModel:
 	#Represents base_model of HBnB project
 
 
-	def __init__(self, id, created_at, updated_at):
+	def __init__(self):
 		#Public instance attributes
-		self.id = id
-		self.created_at = created_at
-		self.updated_at = updated_at
-
-
-	def code(self):
-		print(f"{self.id}, {self.created_at}, {self.updated_at} is first instance")
-
-
-		instance1 = BaseModel("{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow")
-		instance2 = BaseModel("{str(uuid.uuid4)}", "datetime.utcnow", "datetime.utcnow")
-
-		instance1.code()
-		instance2.code()
-
-
-	def save(self):
-		#Modifies updated_at with current datetime. 
-		self.updated_at = datetime.utcnow()
-		models.storage.save()
-
-
-	def to_dict(self):
-		#Creates a dict of the self-set values
-		return_dict = self.__dict__.copy()
-		return_dict["__class__"] = self.__class__.__name__
-		return_dict["created_at"] = self.created_at.isoformat()
-		return_dict["updated_at"] = self.updated_at.isoformat()
-		return return_dict
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = self.created_at
 
 
 	def __str__(self):
 		#Returns str representation of the BaseModel
-		class_name = self.__class__.__name__
-		return "[{}] ({}) {}".format(class_name, self.id, self._dict_)
+		class_name = self.__class__.__name
+		return f"[{self._class_._name_}] ({self.id}) {self._dict_}"
+
+
+	def save(self):
+		#Modifies updated_at with current datetime. 
+		self.updated_at = datetime.now()
+
+
+	def to_dict(self):
+		#Creates a dict of the self-set values
+		dict_repr = self.__dict__.copy()
+		dict_repr["__class__"] = self.__class__.__name__
+		dict_repr["created_at"] = self.created_at.isoformat()
+		dict_repr["updated_at"] = self.updated_at.isoformat()
+		return dict_repr
